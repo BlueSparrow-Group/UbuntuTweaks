494e0e4d0205e0364b056d09e890dbf323cc6d7d
sparrow3278 <sparrow3278@icloud.com>
2023-09-11 11:13:31 +0200

    Screensaver lock delay change

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ cd $(dirname $0)
 sudo apt update
 
 # Install software and libs from ubuntu repositories
-sudo apt install -y ca-certificates curl gnupg apt-transport-https unzip git snapd openjdk-17-jre openjdk-17-jre libfuse2 libreoffice gimp gimp-data-extras gimp-help-common inkscape inkscape-open-symbols inkscape-tutorials kdenlive blender handbrake filezilla obs-studio codeblocks codeblocks-common codeblocks-contrib codeblocks-dev libcodeblocks0 thonny arduino dconf-cli dconf-editor mc
+sudo apt install -y ca-certificates curl gnupg software-properties-common apt-transport-https unzip git snapd openjdk-17-jre openjdk-17-jre libfuse2 libreoffice gimp gimp-data-extras gimp-help-common inkscape inkscape-open-symbols inkscape-tutorials kdenlive blender handbrake filezilla obs-studio codeblocks codeblocks-common codeblocks-contrib codeblocks-dev libcodeblocks0 thonny arduino dconf-cli dconf-editor mc
 
 # Install software from snap
 sudo snap install chromium
@@ -31,6 +31,10 @@ sudo dpkg -i /tmp/ndi.deb
 sudo apt --fix-broken install
 sudo dpkg -i /tmp/obs-ndi.deb
 sudo apt --fix-broken install -y
+
+# Add MS Edge repository
+echo 'deb [signed-by=/usr/share/keyrings/microsoft-edge.gpg] https://packages.microsoft.com/repos/edge stable main' | sudo tee /etc/apt/sources.list.d/microsoft-edge.list
+sudo wget -O- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /usr/share/keyrings/microsoft-edge.gpg
 
 # Add Lightworks repository
 sudo rm -R /tmp/lightworks.deb
@@ -76,7 +80,7 @@ sudo /tmp/ose/cert_install/cert_install.sh
 sudo apt update
 
 # Install software from added repositories
-sudo apt install -y texstudio anydesk docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin unityhub
+sudo apt install -y microsoft-edge-stable texstudio anydesk docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin unityhub
 
 # Copy background file
 sudo cp bg.jpg /opt/bg.jpg
@@ -104,8 +108,8 @@ sudo apt upgrade -y
 sudo mkdir -p /etc/dconf/profile
 sudo sh -c 'echo "user-db:user\nsystem-db:local">/etc/dconf/profile/user'
 sudo mkdir -p /etc/dconf/db/local.d/locks
-sudo sh -c $'echo "[org/gnome/desktop/background]\n\n# URI to use for the background image\npicture-uri=\'file:///opt/bg.jpg\'\n\n# Specify one of the rendering options for the background image:\npicture-options=\'zoom\'\n\n# Specify the left or top color when drawing gradients, or the solid color\nprimary-color=\'2E405D\'\n\n# Specify the right or bottom color when drawing gradients\nsecondary-color=\'DFEAF7\'\n\n[org/gnome/desktop/screensaver]\n\n# URI to use for the background image\npicture-uri=\'file:///opt/bg.jpg\'\n\n# Specify one of the rendering options for the background image:\npicture-options=\'zoom\'\n\n# Specify the left or top color when drawing gradients, or the solid color\nprimary-color=\'2E405D\'\n\n# Specify the right or bottom color when drawing gradients\nsecondary-color=\'DFEAF7\'\nlogout-enabled=true\n\n[org/gnome/online-accounts]\n\n# Disable gnome online accounts\nwhitelisted-providers=[\'\']\n\n[org/gnome/shell/extensions/dash-to-dock]\n\nextend-height=false\ndock-position=\'BOTTOM\'\nshow-apps-at-top=true\n\n[org/gnome/desktop/interface]\n\ngtk-theme=\'WhiteSur-Light\'\nicon-theme=\'WhiteSur-light\'\n\n[org/gnome/desktop/wm/preferences]\n\ntheme=\'WhiteSur-Light\'">/etc/dconf/db/local.d/00-lockdown'
-sudo sh -c $'echo "# Prevent changes to the following keys:\n\n/org/gnome/desktop/background/picture-uri\n/org/gnome/desktop/background/picture-options\n/org/gnome/desktop/background/primary-color\n/org/gnome/desktop/background/secondary-color\n/org/gnome/desktop/screensaver/picture-uri\n/org/gnome/desktop/screensaver/picture-options\n/org/gnome/desktop/screensaver/primary-color\n/org/gnome/desktop/screensaver/secondary-color\n/org/gnome/desktop/screensaver/logout-enabled\n/org/gnome/online-accounts/whitelisted-providers\norg.gnome.shell.extensions.dash-to-dock.extend-height\norg.gnome.shell.extensions.dash-to-dock.dock-position\norg.gnome.shell.extensions.dash-to-dock.show-apps-at-top\norg.gnome.desktop.interface.gtk-theme\norg.gnome.desktop.interface.icon-theme\norg.gnome.desktop.wm.preferences.theme">/etc/dconf/db/local.d/locks/lockdown'
+sudo sh -c $'echo "[org/gnome/desktop/background]\n\n# URI to use for the background image\npicture-uri=\'file:///opt/bg.jpg\'\n\n# Specify one of the rendering options for the background image:\npicture-options=\'zoom\'\n\n# Specify the left or top color when drawing gradients, or the solid color\nprimary-color=\'2E405D\'\n\n# Specify the right or bottom color when drawing gradients\nsecondary-color=\'DFEAF7\'\n\n[org/gnome/desktop/screensaver]\n\n# URI to use for the background image\npicture-uri=\'file:///opt/bg.jpg\'\n\n# Specify one of the rendering options for the background image:\npicture-options=\'zoom\'\n\n# Specify the left or top color when drawing gradients, or the solid color\nprimary-color=\'2E405D\'\n\n# Specify the right or bottom color when drawing gradients\nsecondary-color=\'DFEAF7\'\nlogout-enabled=true\nlock-delay=360\n\n[org/gnome/online-accounts]\n\n# Disable gnome online accounts\nwhitelisted-providers=[\'\']\n\n[org/gnome/shell/extensions/dash-to-dock]\n\nextend-height=false\ndock-position=\'BOTTOM\'\nshow-apps-at-top=true\n\n[org/gnome/desktop/interface]\n\ngtk-theme=\'WhiteSur-Light\'\nicon-theme=\'WhiteSur-light\'\n\n[org/gnome/desktop/wm/preferences]\n\ntheme=\'WhiteSur-Light\'">/etc/dconf/db/local.d/00-lockdown'
+sudo sh -c $'echo "# Prevent changes to the following keys:\n\n/org/gnome/desktop/background/picture-uri\n/org/gnome/desktop/background/picture-options\n/org/gnome/desktop/background/primary-color\n/org/gnome/desktop/background/secondary-color\n/org/gnome/desktop/screensaver/picture-uri\n/org/gnome/desktop/screensaver/picture-options\n/org/gnome/desktop/screensaver/primary-color\n/org/gnome/desktop/screensaver/secondary-color\n/org/gnome/desktop/screensaver/logout-enabled\n/org/gnome/desktop/screensaver/lock-delay\n/org/gnome/online-accounts/whitelisted-providers\norg/gnome/shell/extensions/dash-to-dock/extend-height\norg/gnome/shell/extensions/dash-to-dock/dock-position\norg/gnome/shell/extensions/dash-to-dock/show-apps-at-top\norg/gnome/desktop/interface/gtk-theme\norg/gnome/desktop/interface/icon-theme\norg/gnome/desktop/wm/preferences/theme">/etc/dconf/db/local.d/locks/lockdown'
 sudo dconf update
 
 # Disable access to settings app for non-admin users

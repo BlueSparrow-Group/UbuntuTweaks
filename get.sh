10bc3c27fb3c10e25016f11a3a912a10429a3969
Adam Wróbel <adam.wrobel@bluesparrow.pl>
2024-01-17 19:46:58 +0100

    Fix script syntax

--- get.sh
+++ get.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/bin/sh -c "$(curl -fsSL https://gitlab.com/bluesparrow/ubuntutweaks/-/raw/v1/main.sh)" -o "install-self"
+/bin/sh -c "$(curl -fsSL https://gitlab.com/bluesparrow/ubuntutweaks/-/raw/v1/main.sh)" $0 "install-self"

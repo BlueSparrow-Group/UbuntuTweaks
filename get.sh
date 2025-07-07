b21e3996f220d0edc389fb64b3ea3f73cffc70a0
pikoseq9 <wekpat@wp.pl>
2025-07-07 09:59:38 +0200

    fix path

--- get.sh
+++ get.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/BlueSparrow-Group/UbuntuTweaks/v1/main.sh)" $0 "install-self"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/BlueSparrow-Group/UbuntuTweaks/dev/main.sh)" $0 "install-self"
